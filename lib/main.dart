6631272f0b777405b2bdc8ffa8dafc69da358102
Amber Jain <jainanuamber@gmail.com>
2022-06-01 03:10:13 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:incredible_india/screens/home_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Incredible India',
       theme: ThemeData(primaryColor: Colors.white),
+      home: HomeScreen(),
     );
   }
 }
