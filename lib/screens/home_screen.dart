e194498256d6049bcc0b4abc14b7c775668bfc64
Amber Jain <jainanuamber@gmail.com>
2022-06-01 03:43:42 +0530

    updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:incredible_india/data/photos_data.dart';
 import 'package:incredible_india/model/photos_model.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -31,28 +33,50 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-        elevation: 0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "incredible",
-              style: TextStyle(
-                  color: Colors.orange.shade900, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              width: 5,
-            ),
-            Text(
-              "india",
-              style: TextStyle(
-                  color: Colors.green.shade900, fontWeight: FontWeight.bold),
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          elevation: 0,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "incredible",
+                style: TextStyle(
+                    color: Colors.amber.shade900, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Text(
+                "india",
+                style: TextStyle(
+                    color: Colors.green.shade900, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+        body: _loading
+            ? Center(
+                child: CircularProgressIndicator(color: Colors.teal.shade700))
+            : Container(
+                padding: const EdgeInsets.all(10),
+                child: StaggeredGridView.countBuilder(
+                  crossAxisCount: 4,
+                  itemCount: photoList.length,
+                  itemBuilder: (context, index) {
+                    return ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.network(
+                        photoList[index].webformatURL,
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                  staggeredTileBuilder: (int i) =>
+                      StaggeredTile.count(2, i.isEven ? 2 : 3),
+                  mainAxisSpacing: 8.0,
+                  crossAxisSpacing: 8.0,
+                ),
+              ));
   }
 }
