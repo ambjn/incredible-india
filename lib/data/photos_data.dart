f7f6033210ca547170ea9bd8c2a40cac490f0b77
Amber Jain <jainanuamber@gmail.com>
2022-06-01 03:05:35 +0530

    updated

---
+++ lib/data/photos_data.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:incredible_india/model/photos_model.dart';
+import 'package:incredible_india/info.dart';
+
+class Photos {
+  List<PhotoModel> photos = [];
+
+  Future<void> getPhotos() async {
+    var url = Uri.parse(
+        'https://pixabay.com/api/?key=${API_KEY}&q=india&image_type=photo&pretty=true&per_page=100');
+
+    var response = await http.get(url);
+    var jsonData = jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      // specifies everything is OK
+      jsonData['hits'].forEach((element) {
+        if (element['webformatURL'] != null) {
+          PhotoModel photoModel =
+              PhotoModel(webformatURL: element['webformatURL']);
+
+          photos.add(photoModel);
+        }
+      });
+    }
+  }
+}
